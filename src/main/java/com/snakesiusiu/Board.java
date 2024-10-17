1a73ae724ab8227c9c208465af62bedb895c8d15
AcidTSB <acidg694@hmail.com>
2024-10-17 17:49:26 +0700

    Cap nhat nut start restart va score

--- src/main/java/com/snakesiusiu/Board.java
+++ src/main/java/com/snakesiusiu/Board.java
@@ -11,7 +11,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -30,36 +36,50 @@ public class Board extends JPanel implements ActionListener {
     private int dots;
     private int apple_x;
     private int apple_y;
+    private int score;
+    private int highScore;
 
     private boolean leftDirection = false;
     private boolean rightDirection = true;
     private boolean upDirection = false;
     private boolean downDirection = false;
-    private boolean inGame = true;
+    private boolean inGame = false;
 
     private Timer timer;
     private Image ball;
     private Image apple;
     private Image head;
+    private JButton startButton;
+    private JButton restartButton;
+    
 
     public Board() {
-        
         initBoard();
     }
     
     private void initBoard() {
-
         addKeyListener(new TAdapter());
         setBackground(Color.black);
         setFocusable(true);
-
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
         loadImages();
-        initGame();
+        loadHighScore();
+        
+        startButton = new JButton("Start");
+        startButton.setBounds(B_WIDTH / 2 - 50, B_HEIGHT / 2 - 25, 100, 50);
+        startButton.addActionListener(e -> startGame());
+        add(startButton);
+        
+        restartButton = new JButton("Restart");
+        restartButton.setBounds(B_WIDTH / 2 - 50, B_HEIGHT / 2 - 25, 100, 50);
+        restartButton.addActionListener(e -> restartGame());
+        restartButton.setVisible(false);
+        add(restartButton);
+        
+        setLayout(null);
     }
 
     private void loadImages() {
-
         ImageIcon iid = new ImageIcon("src/main/resources/dot.png");
         ball = iid.getImage();
 
@@ -70,8 +90,22 @@ public class Board extends JPanel implements ActionListener {
         head = iih.getImage();
     }
 
-    private void initGame() {
+    private void startGame() {
+        startButton.setVisible(false);
+        restartButton.setVisible(false);
+        inGame = true;
+        score = 0;
+        initGame();
+    }
 
+    private void restartGame() {
+        restartButton.setVisible(false);
+        inGame = true;
+        score = 0;
+        initGame();
+    }
+
+    private void initGame() {
         dots = 3;
 
         for (int z = 0; z < dots; z++) {
@@ -81,6 +115,9 @@ public class Board extends JPanel implements ActionListener {
         
         locateApple();
 
+        if (timer != null) {
+            timer.stop();
+        }
         timer = new Timer(DELAY, this);
         timer.start();
     }
@@ -88,14 +125,11 @@ public class Board extends JPanel implements ActionListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         doDrawing(g);
     }
     
     private void doDrawing(Graphics g) {
-        
         if (inGame) {
-
             g.drawImage(apple, apple_x, apple_y, this);
 
             for (int z = 0; z < dots; z++) {
@@ -106,16 +140,36 @@ public class Board extends JPanel implements ActionListener {
                 }
             }
 
+            drawScore(g);
+            drawHighScore(g);
+
             Toolkit.getDefaultToolkit().sync();
-
         } else {
-
             gameOver(g);
         }        
     }
 
+    private void drawScore(Graphics g) {
+        String msg = "Score: " + score;
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metr = getFontMetrics(small);
+
+        g.setColor(Color.white);
+        g.setFont(small);
+        g.drawString(msg, 10, 20);
+    }
+
+    private void drawHighScore(Graphics g) {
+        String msg = "High Score: " + highScore;
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metr = getFontMetrics(small);
+
+        g.setColor(Color.white);
+        g.setFont(small);
+        g.drawString(msg, 10, 40);
+    }
+
     private void gameOver(Graphics g) {
-        
         String msg = "Game Over";
         Font small = new Font("Helvetica", Font.BOLD, 14);
         FontMetrics metr = getFontMetrics(small);
@@ -123,19 +177,24 @@ public class Board extends JPanel implements ActionListener {
         g.setColor(Color.white);
         g.setFont(small);
         g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
+
+        if (score > highScore) {
+            highScore = score;
+            saveHighScore();
+        }
+
+        restartButton.setVisible(true);
     }
 
     private void checkApple() {
-
         if ((x[0] == apple_x) && (y[0] == apple_y)) {
-
             dots++;
+            score += 10;
             locateApple();
         }
     }
 
     private void move() {
-
         for (int z = dots; z > 0; z--) {
             x[z] = x[(z - 1)];
             y[z] = y[(z - 1)];
@@ -159,9 +218,7 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void checkCollision() {
-
         for (int z = dots; z > 0; z--) {
-
             if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
                 inGame = false;
             }
@@ -189,7 +246,6 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void locateApple() {
-
         int r = (int) (Math.random() * RAND_POS);
         apple_x = ((r * DOT_SIZE));
 
@@ -197,11 +253,25 @@ public class Board extends JPanel implements ActionListener {
         apple_y = ((r * DOT_SIZE));
     }
 
+    private void loadHighScore() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("highscore.txt"))) {
+            highScore = Integer.parseInt(reader.readLine());
+        } catch (IOException | NumberFormatException e) {
+            highScore = 0;
+        }
+    }
+
+    private void saveHighScore() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("highscore.txt"))) {
+            writer.write(String.valueOf(highScore));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if (inGame) {
-
             checkApple();
             checkCollision();
             move();
@@ -211,10 +281,8 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private class TAdapter extends KeyAdapter {
-
         @Override
         public void keyPressed(KeyEvent e) {
-
             int key = e.getKeyCode();
 
             if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
