f030fd9101129de846574c480f841ca0f5363d28
Khoi0323 <2051150134@ut.edu.vn>
2024-11-23 13:49:48 +0700

    Update GameModel.java

--- src/main/java/com/snakesiusiu/model/GameModel.java
+++ src/main/java/com/snakesiusiu/model/GameModel.java
@@ -8,39 +8,39 @@ public class GameModel {
     private final int DOT_SIZE = 10;
     private final int ALL_DOTS = 900;
     private final int RAND_POS = 29;
-    
+
     private int delay = 240;
     private final int[] x = new int[ALL_DOTS];
     private final int[] y = new int[ALL_DOTS];
-    
+
     private int dots;
     private int appleX;
     private int appleY;
     private int score;
     private int highScore;
-    
+
     private boolean leftDirection = false;
     private boolean rightDirection = true;
     private boolean upDirection = false;
     private boolean downDirection = false;
     private boolean inGame = false;
     private boolean isPaused = false;
-    
+
     public GameModel() {
         loadHighScore();
     }
-    
+
     public void initGame() {
         dots = 3;
         score = 0;
-        
+
         for (int z = 0; z < dots; z++) {
             x[z] = 50 - z * 10;
             y[z] = 50;
         }
-        
+
         locateApple();
-        
+
         leftDirection = false;
         rightDirection = true;
         upDirection = false;
@@ -48,13 +48,13 @@ public class GameModel {
         inGame = true;
         isPaused = false;
     }
-    
+
     public void move() {
         for (int z = dots; z > 0; z--) {
             x[z] = x[z - 1];
             y[z] = y[z - 1];
         }
-        
+
         if (leftDirection) {
             x[0] -= DOT_SIZE;
         }
@@ -68,7 +68,7 @@ public class GameModel {
             y[0] += DOT_SIZE;
         }
     }
-    
+
     public void checkApple() {
         if ((x[0] == appleX) && (y[0] == appleY)) {
             dots++;
@@ -76,31 +76,31 @@ public class GameModel {
             locateApple();
         }
     }
-    
+
     public void checkCollision() {
         for (int z = dots; z > 0; z--) {
             if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
                 inGame = false;
             }
         }
-        
+
         if (y[0] >= B_HEIGHT || y[0] < 0 || x[0] >= B_WIDTH || x[0] < 0) {
             inGame = false;
         }
-        
+
         if (!inGame && score > highScore) {
             highScore = score;
             saveHighScore();
         }
     }
-    
+
     private void locateApple() {
         int r = (int) (Math.random() * RAND_POS);
         appleX = r * DOT_SIZE;
         r = (int) (Math.random() * RAND_POS);
         appleY = r * DOT_SIZE;
     }
-    
+
     private void loadHighScore() {
         try (BufferedReader reader = new BufferedReader(new FileReader("highscore.txt"))) {
             highScore = Integer.parseInt(reader.readLine());
@@ -108,7 +108,7 @@ public class GameModel {
             highScore = 0;
         }
     }
-    
+
     private void saveHighScore() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("highscore.txt"))) {
             writer.write(String.valueOf(highScore));
@@ -116,24 +116,68 @@ public class GameModel {
             e.printStackTrace();
         }
     }
-    
+
     // Getters and setters
-    public int getBWidth() { return B_WIDTH; }
-    public int getBHeight() { return B_HEIGHT; }
-    public int getDotSize() { return DOT_SIZE; }
-    public int[] getX() { return x; }
-    public int[] getY() { return y; }
-    public int getDots() { return dots; }
-    public int getAppleX() { return appleX; }
-    public int getAppleY() { return appleY; }
-    public int getScore() { return score; }
-    public int getHighScore() { return highScore; }
-    public boolean isInGame() { return inGame; }
-    public boolean isPaused() { return isPaused; }
-    public void setPaused(boolean paused) { isPaused = paused; }
-    public void setDelay(int delay) { this.delay = delay; }
-    public int getDelay() { return delay; }
-    
+    public int getBWidth() {
+        return B_WIDTH;
+    }
+
+    public int getBHeight() {
+        return B_HEIGHT;
+    }
+
+    public int getDotSize() {
+        return DOT_SIZE;
+    }
+
+    public int[] getX() {
+        return x;
+    }
+
+    public int[] getY() {
+        return y;
+    }
+
+    public int getDots() {
+        return dots;
+    }
+
+    public int getAppleX() {
+        return appleX;
+    }
+
+    public int getAppleY() {
+        return appleY;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public boolean isInGame() {
+        return inGame;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
+    }
+
+    public void setPaused(boolean paused) {
+        isPaused = paused;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
     public void setDirection(Direction direction) {
         switch (direction) {
             case LEFT:
